4e6271ee57572d26012244d58a1c1216b125545d
Ronnie Bjørkelund <ronnie.bjorkelund@gmail.com>
2022-02-07 01:33:43 +0100

    messing with docker

--- server/config/databaseconfig.js
+++ server/config/databaseconfig.js
@@ -1,17 +1,20 @@
 import mysql from 'mysql';
+import 'dotenv/config'; 
 
 const createDatabase = () => {
 
   const conn = mysql.createConnection({
     host     : process.env.HOST,
+    port     : process.env.MYSQL_LOCAL_PORT,
     user     : process.env.MYSQL_USER,
     password : process.env.MYSQL_PASSWORD,
   });
-  
-  console.log('Uncomment code to create database');
-  console.log(conn);
 
-  /*
+  console.log('*********');
+  console.log(process.env.MYSQL_USER + ' ' + process.env.MYSQL_PASSWORD);
+  console.log(process.env.HOST + ' ' + process.env.MYSQL_PASSWORD);
+  console.log('*********');
+  
   conn.connect(function(err) {
     if (err) {
       console.error('error connecting: ' + err.stack);
@@ -20,7 +23,7 @@ const createDatabase = () => {
   
     console.log('connected as id ' + conn.threadId);
   });
-  */
+  
   // con.connect(function(err) {
   //   if (err) throw err;
   //   console.log('Connected to mysql server!');
